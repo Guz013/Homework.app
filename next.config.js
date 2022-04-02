d6e86038b7a2eb440f02efe949566193f3e03c82
Guz <43732358+Guz013@users.noreply.github.com>
2022-04-02 16:47:03 -0300

    project-structure

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+const withPreact = require('next-plugin-preact');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-})
+module.exports = withPreact(
+	withPWA({
+		pwa: {
+			dest: 'public',
+			runtimeCaching,
+		},
+	})
+);
